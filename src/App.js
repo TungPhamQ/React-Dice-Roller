17a8f5251704fa141079a81818a962189db0d118
TungPhamQ <tungpham2109@gmail.com>
2024-01-14 05:49:41 +0700

    [fe]make custom dice roller

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Dice from "react-dice-roll";
 
 function App() {
+  const [diceAmount, setDiceAmount] = useState(1);
+  const [lockDiceList, setLockDiceList] = useState([]);
+  const diceRefs = Array.from({ length: diceAmount }, () => React.createRef());
+
+  const handleRollAllButtonClick = () => {
+    if (diceAmount === lockDiceList.length) alert("All dice is locked");
+    diceRefs.forEach((ref, index) => {
+      if (!lockDiceList.includes(index) && ref.current) {
+        ref.current.rollDice();
+      }
+    });
+  };
+  const handleInputChange = (event) => {
+    setDiceAmount(event.target.value);
+  };
+
+  const onLockDice = (diceIndex) => {
+    const isDiceLocked = lockDiceList.includes(diceIndex);
+    if (!isDiceLocked) {
+      setLockDiceList([...lockDiceList, diceIndex]);
+    } else {
+      setLockDiceList(lockDiceList.filter((el) => el !== diceIndex));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Enter Dice Amount</h3>
+      <input
+        type="text"
+        value={diceAmount}
+        onChange={handleInputChange}
+        placeholder="Enter dice amount ..."
+      />
+      <div className="container">
+        {diceRefs.map((ref, index) => (
+          <div className={`dice-wrapper`} onClick={() => onLockDice(index)}>
+            <div
+              className={`${lockDiceList.includes(index) && "locked-border"}  `}
+            ></div>
+            <Dice key={index} size={100} ref={ref} triggers={[""]} />
+          </div>
+        ))}
+      </div>
+      <button className="roll-btn" onClick={handleRollAllButtonClick}>
+        ROLL
+      </button>
     </div>
   );
 }
